0716b2a6660b15f89c0915d0d35f7725dbd8960b
Maksim Kniha <maksimbeta777@mail.ru>
2017-04-02 19:33:22 +0300

    Set up the virtual environment, django

---
+++ social_network_project/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\gitsocialnetwork\socialnetwork\social_network_project\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
